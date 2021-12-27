7e5b0176b161e4024749c9e9acb0111dadda50a0
Kevin D Goveas <kevdanngovead@gmail.com>
2021-12-27 19:10:01 +0530

    :sparkles: Added Home Page

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,3 +1,13 @@
+<script>
+</script>
+
 <div class="about">
-	<p>Kevin Daniel Goveas</p>
+	<div class="about__typography">
+		<p class="about__typography__name">Kevin Daniel Goveas</p>
+		<p class="about__typography__title">Fullstack Web Developer</p>
+		<p class="about__typography__quote">A Well Written Piece of Code is a Thing of Beauty</p>
+	</div>
+	<div class="about__image">
+		<img src="/Me.svg" alt="Kevin" />
+	</div>
 </div>
