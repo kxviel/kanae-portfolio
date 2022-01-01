2587abcaf1cd5d140aa06edb8453677e7eed5d5b
Kevin D Goveas <kevdanngovead@gmail.com>
2022-01-01 17:15:33 +0530

    :lipstick: UI changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,13 @@
 	import '../styles/app.scss';
 	import Navbar from '../layouts/Navbar.svelte';
 	import About from '../components/About.svelte';
+	import Skills from '../components/Skills.svelte';
 </script>
 
-<main>
-	<Navbar />
-	<About />
-</main>
+<div>
+	<div class="root">
+		<Navbar />
+		<About />
+	</div>
+	<Skills />
+</div>
